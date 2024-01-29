1bd8102962253bd04e911bb828c552656052e7fb
Viraj Abeywardana <abeywardana21@gmail.com>
2024-01-29 23:32:35 +0530

    fetching data

--- app.js
+++ app.js
@@ -1,13 +1,33 @@
 const express = require('express')
+const { connectToDb, getDb} = require('./db')
 
 //init app & middleware
 const app = express();
 
-app.listen(3000 , () => {
-    console.log("app listening on port 3000");
-});
+//db 
+let db;
+
+connectToDb((err) => {
+    if(!err){
+        app.listen(3000 , () => {
+            console.log("app listening on port 3000");
+        })
+        db = getDb();
+    }
+})
+
 
 //routes
 app.get('/books', (req, res) => {
-    res.json({msg: "Welcome to the API"});
+    let books = [];
+    db.collection('books')
+        .find() //cursor toArray forEach
+        .sort({author: 1})
+        .forEach(book => books.push(book))
+        .then(() => {
+            res.status(200).json(books)
+        })
+        .catch(() => {
+            res.status(500).json({error:'Could not fetch the documents'})
+        })
 })
