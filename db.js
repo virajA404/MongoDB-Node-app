bbb54ad70e995d99cf9bb8bd2af475460fa28e1b
Viraj Abeywardana <abeywardana21@gmail.com>
2024-01-29 23:12:25 +0530

    create code for db

---
+++ db.js
@@ -0,0 +1,21 @@
+//destructuring the client obj. MongoClient helps to connect to db 
+const { MongoClient } = require('mongodb');
+
+let dbConnection;
+
+module.exports = {
+    //connect to a db
+    connectToDb: (cb) => {
+        MongoClient.connect('mongodb://localhost:27017/bookstore')
+            .then((client) => {
+                dbConnection = client.db()
+                return cb()
+            })
+            .catch(err => {
+                console.log(err);
+                return cb()
+            })
+    },
+    //retrieve db connection after connecting
+    getDb: () => dbConnection
+}
