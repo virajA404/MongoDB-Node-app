2c650e369bec1d550bb09564953e3f7517d9420c
Viraj Abeywardana <abeywardana21@gmail.com>
2024-02-02 11:12:25 +0530

    changing fetch all books code to es6

--- app.js
+++ app.js
@@ -1,34 +1,50 @@
-import express  from 'express';
-import {connectToDb,getDb} from './db.js';
+import express from "express";
+import { connectToDb, getDb } from "./db.js";
+import { ObjectId } from "mongodb";
+
 
 //init app & middleware
 const app = express();
-
 
 //connecting to db
 let db;
 
 connectToDb((err) => {
-    if(!err){
-        app.listen(3000 , () => {
+    if (!err) {
+        app.listen(3000, () => {
             console.log("app listening on port 3000");
-        })
+        });
         db = getDb();
     }
-})
+});
 
-
-//routes
-app.get('/books', (req, res) => {
+//Fetch all the books
+app.get("/books", async (req, res) => {
     let books = [];
-    db.collection('books')
-        .find() //cursor toArray forEach
-        .sort({author: 1})
-        .forEach(book => books.push(book))
-        .then(() => {
-            res.status(200).json(books)
-        })
-        .catch(() => {
-            res.status(500).json({error:'Could not fetch the documents'})
-        })
-})
+
+    try {
+        const cursor = await db.collection("books").find().sort({ author: 1 });
+
+        await cursor.forEach((book) => {
+            books.push(book);
+        });
+        res.status(200).json(books);
+    } catch (error) {
+        res.status(500).json({ error: "Could not fetch the documents" });
+    }
+
+    //OLD SYNTAX
+    // let books = [];
+    // db.collection('books')
+    //     .find() //cursor toArray forEach
+    //     .sort({author: 1})
+    //     .forEach(book => books.push(book))
+    //     .then(() => {
+    //         res.status(200).json(books)
+    //     })
+    //     .catch(() => {
+    //         res.status(500).json({error:'Could not fetch the documents'})
+    //     })
+});
+
+
